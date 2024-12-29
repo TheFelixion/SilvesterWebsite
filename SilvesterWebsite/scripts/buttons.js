1c36c8dcc81b434128ec9853f55938ae6feb88b0
TheFelixion <132163486+TheFelixion@users.noreply.github.com>
2024-12-29 02:08:03 +0100

    HTML + CSS

---
+++ SilvesterWebsite/scripts/buttons.js
@@ -0,0 +1,4 @@
+function playFireworkrocketSound() {
+    let fireworkrocketSound = new Audio("../SilvesterWebsite/res/audios/mixkit-fast-whistle-firework-3103.mp3");
+    fireworkrocketSound.play();
+}
